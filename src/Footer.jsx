561ed5514b066750f830371c0252f599cc10b3b6
alherdom <alherdom@outlook.com>
2024-02-09 20:25:05 +0000

    ✅ Complete.

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,9 +5,15 @@ const Footer = () => {
   return (
     <Container>
       <Typography textAlign="center" sx={{ mt: 2 }}>
-        Powered by:{" "}
+        Powered by{" "}
         <a href="https://www.weatherapi.com/" title="Weather API">
           WeatherAPI.com
+        </a>
+      </Typography>
+      <Typography textAlign="center" sx={{ mt: 2 }}>
+        Created by{" "}
+        <a href="https://github.com/alherdom" title="GitHub">
+          @alherdom
         </a>
       </Typography>
     </Container>
