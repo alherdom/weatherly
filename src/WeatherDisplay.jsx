06f9829c2a90898e92bb46a9b3b62e44efcf602a
alherdom <alherdom@outlook.com>
2024-02-09 20:55:20 +0000

    ✅ Complete.

--- src/WeatherDisplay.jsx
+++ src/WeatherDisplay.jsx
@@ -19,7 +19,7 @@ const WeatherDisplay = ({ weather }) => {
         sx={{
           display: "inline-flex",
           alignItems: "center",
-          justifyContent: "space-between",
+          justifyContent: "center",
           textAlign: "center",
         }}
       >
